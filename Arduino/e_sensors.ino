c5b02268f3ecba3aa505e96e1754839f981b12d4
James Moore <hello@jmoore.me>
2013-03-20 19:28:16 -0700

    bee counter

--- Arduino/e_sensors.ino
+++ Arduino/e_sensors.ino
@@ -27,3 +27,38 @@ void readMic(int port, int number)
   addKVPair(p("MIC_%i",number), analogRead(port));
 }
 
+void readBeeCounter(int number)
+{
+  Wire.requestFrom(number, 9);
+
+  int totalIn = 0;
+  int totalOut = 0;
+  char inChar[5];
+  char outChar[5];
+  char colon;
+
+  if (Wire.available()) {
+    
+    inChar[0] = Wire.read();
+    inChar[1] = Wire.read();
+    inChar[2] = Wire.read();
+    inChar[3] = Wire.read();
+    inChar[4] = 0x0;
+    
+    colon = Wire.read();
+
+    outChar[0] = Wire.read();
+    outChar[1] = Wire.read();
+    outChar[2] = Wire.read();
+    outChar[3] = Wire.read();
+    outChar[4] = 0x0;
+    
+    totalIn = atol(inChar);
+    totalOut = atol(outChar);
+
+  }
+  
+  addKVPair(p("BC_%i_IN",number), totalIn);
+  addKVPair(p("BC_%i_OUT",number), totalOut);
+
+}
