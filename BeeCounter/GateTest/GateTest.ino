ec9209e46094d5a35f6daf1bf789f3fc1ce3deea
James Moore <hello@jmoore.me>
2013-04-22 18:30:52 -0700

    formatting

--- BeeCounter/GateTest/GateTest.ino
+++ BeeCounter/GateTest/GateTest.ino
@@ -1,7 +1,6 @@
-const int numberOfGates = 22;
+const int numberOfGates = 21;
 const int debeebounce = 300;
 const int outputDelay = 1000;
-unsigned long lastOutput = 0;
 unsigned long now = 0;
 
 typedef struct
@@ -26,77 +25,82 @@ typedef struct
   int inTotal;
   int outTotal;
 
-  } BeeCounter;
+} 
+BeeCounter;
 
-  BeeCounter bc = {
-    "abcdefghijklnopqrtuvwx", // gate letters
-    {44,42,40,38,16,14,12,10,8 ,35,34,33,32,19,21,23,25,7 ,5 ,3 ,2 ,0 }, // inPins
-    {45,43,41,39,17,15,13,11,9 ,31,30,29,28,18,20,22,24,26,6 ,4 ,1 ,27} // outPins
-  };
+BeeCounter bc = {
+  "abcdefghijlnopqrtuvwx", // gate letters
+//  A  B  C  D  E  F  G  H  I  J  L  N  O  P  Q  R  T  U  V  W   X
+  { 44,42,40,38,16,14,12,10,8 ,35,33,32,19,21,23,25,7 ,5 ,3 ,2  ,36   }, // inPins
+  { 45,43,41,39,17,15,13,11,9 ,31,29,28,18,20,22,24,26,6 ,4 ,37 ,27  } // outPins
+};
 
-  void arrayPrint(int array[])
-  {
+void arrayPrint(int array[])
+{
 
-    for (int i = 0; i <= numberOfGates; i++) {
-      Serial.print(array[i]);
-      Serial.print(" ");
-    }
-    Serial.println();
+  for (int i = 0; i < numberOfGates; i++) {
+    Serial.print(array[i]);
+    Serial.print(" ");
   }
+  Serial.println();
+}
 
-  void setup()
-  {
-    Serial.begin(57600);
+void beeIn(int gate)
+{
+  bc.inCount[gate]++;
+  bc.inTotal++;
+  Serial.print("Bee In on gate ");
+  Serial.println(bc.gates[gate]);
+}
+
+void beeOut(int gate)
+{
+  bc.outCount[gate]++;
+  bc.outTotal++;
+  Serial.print("Bee Out on gate ");
+  Serial.println(bc.gates[gate]);
+}
+
+void setup()
+{
+  Serial.begin(57600);
 
   // Init pins
-    for (int i = 0; i <= numberOfGates; i++) {
-      pinMode(bc.inPin[i], INPUT);
-      pinMode(bc.outPin[i], INPUT);
-    }
+  for (int i = 0; i < numberOfGates; i++) {
+    pinMode(bc.inPin[i], INPUT);
+    pinMode(bc.outPin[i], INPUT);
   }
+}
 
-  void loop()
-  {
+void loop()
+{
 
-    // Loop though the gates
-    for (int i = 0; i <= numberOfGates; i++) {
-      now = millis();
-      // Read Gates
-      bc.inReading[i] = digitalRead(bc.inPin[i]);
-      bc.outReading[i] = digitalRead(bc.outPin[i]);
+  // Loop though the gates
+  for (int i = 0; i < numberOfGates; i++) {
+    now = millis();
+    // Read Gates
+    bc.inReading[i] = digitalRead(bc.inPin[i]);
+    bc.outReading[i] = digitalRead(bc.outPin[i]);
 
-      // Is our reading stable?
-      if (now - bc.lastInTime[i] >= debeebounce && now - bc.lastOutTime[i] >= debeebounce) {
+    // Is our reading stable?
+    if (now - bc.lastInTime[i] >= debeebounce && now - bc.lastOutTime[i] >= debeebounce) {
 
-        // Is the bee going in?
-        if (bc.inReading[i] != bc.lastInReading[i] && bc.inReading[i] > bc.outReading[i]) {
-          bc.inCount[i]++;
-          bc.inTotal++;
-          bc.lastInTime[i] = now;
-        }
-
-        // Is the bee going out?
-        if (bc.outReading[i] != bc.lastOutReading[i] && bc.inReading[i] < bc.outReading[i]) {
-          bc.outCount[i]++;
-          bc.outTotal++;
-          bc.lastOutTime[i] = now;
-        }
+      // Is the bee going in?
+      if (bc.inReading[i] != bc.lastInReading[i] && bc.inReading[i] > bc.outReading[i]) {
+        beeIn(i);
+        bc.lastInTime[i] = now;
       }
 
-      bc.lastInReading[i] = bc.inReading[i];
-      bc.lastOutReading[i] = bc.outReading[i];
-    }
-    
-    if (now - lastOutput >= outputDelay) {
-      for (int i = 0; i <= numberOfGates; i++) {
-        Serial.print(bc.gates[i]);
-        Serial.print(" ");
+      // Is the bee going out?
+      if (bc.outReading[i] != bc.lastOutReading[i] && bc.inReading[i] < bc.outReading[i]) {
+        beeOut(i);
+        bc.lastOutTime[i] = now;
       }
-      Serial.println();
-      arrayPrint(bc.inCount);
-      arrayPrint(bc.outCount);
-      lastOutput = now;
     }
-    
 
+    bc.lastInReading[i] = bc.inReading[i];
+    bc.lastOutReading[i] = bc.outReading[i];
   }
+
+}
+
